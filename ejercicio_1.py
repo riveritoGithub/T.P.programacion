0154164b7f1b635457616a7c1c3f2eda2b5f6eb5
Ricardo <ricardorivero@gmail.com>
2024-09-22 21:11:55 -0300

    TrabajoProgamacion

---
+++ ejercicio_1.py
@@ -0,0 +1,10 @@
+def presentarse():
+    nombre = "ricardo"  # Debe ser una cadena
+    edad = 20           # Debe ser un entero
+    pais = "Argentina"  # Debe ser una cadena
+
+    print(f"¡Hola! Me llamo {nombre}.")
+    print(f"Tengo {edad} años.")
+    print(f"Soy de {pais}.")
+
+
---
+++ ejercicio_4.py
@@ -0,0 +1,5 @@
+def reserva():
+    nombre=input("ingrese su nombre:")
+    dias_de_estadia=int(input("ingrese los dias de entadia")
+    tarifa_diaria=("")
+    in(input("ingrese su nombre{nombre}","dias de estadia{dias_de_estadia}"))
